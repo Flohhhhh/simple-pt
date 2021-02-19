4261e3bcd130c03f97d0a2b9750fcfe45ea4994a
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2021-02-18 23:24:15 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,18 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
--- Tell FiveM's NUI system what the main html file is for this resource
+author 'Floh'
+description 'Simple "peacetime" script for RP servers which disables firing and shows status.'
+version '1.0.0'
+
 ui_page "ui/ui.html"
 
--- Add the files that need to be used/loaded
 files {
 	"ui/ui.html",
 	"ui/ui.js",
 	"ui/ui.css",
 }
 
--- Initiate the clientside lua script
 client_scripts {
 	'client.lua',
 }
