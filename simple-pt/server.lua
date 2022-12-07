80fa2e209c1c0616f02618b722fc68ecaa2246e2
Flohhhhh <camgus322@gmail.com>
2022-12-07 10:13:04 -0500

    Nothing really

--- simple-pt/server.lua
+++ simple-pt/server.lua
@@ -2,7 +2,7 @@
 ------- Config -------
 ----------------------
 -- whether to use ace perms
-local usePerms = false
+local usePerms = true
 
 -- to change what actions are diabled during peacetime see client.lua line 49
 -------------------
@@ -13,13 +13,15 @@ if not GlobalState.peacetime then
 end
 
 RegisterCommand('pt', function(source)
+    -- print("Using perms: " .. usePerms)
+    -- print("Does player have ace: " .. tostring(IsPlayerAceAllowed(source, 'simple.pt')))
     if not usePerms or IsPlayerAceAllowed(source, 'simple.pt') then
         if GlobalState.peacetime then
-        GlobalState.peacetime = false
-        TriggerClientEvent('pt:disabled', -1)
+            GlobalState.peacetime = false
+            TriggerClientEvent('pt:disabled', -1)
         else
-        GlobalState.peacetime = true
-        TriggerClientEvent('pt:enabled', -1)
+            GlobalState.peacetime = true
+            TriggerClientEvent('pt:enabled', -1)
         end
     end
 end, false)
